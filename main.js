763ef1a1c02a86dd9973ba9b044a5539b2d46820
besdar <verda.paco@protonmail.com>
2020-08-12 11:08:18 +0300

    fix js height calculation error

--- main.js
+++ main.js
@@ -2,6 +2,6 @@
 
 document.getElementById('hider').onclick = function toggle() {
     let el = document.getElementById('hide');
-    el.style.height = (el.style.height === '0px') ? '32px' : '0px';
+    el.style.height = (el.style.height === '0px') ? el.children[0].offsetHeight + 'px' : '0px';
 };
                           
