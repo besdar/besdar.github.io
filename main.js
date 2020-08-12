13008a592d2e6b0fe868b2a7191640364935d49c
besdar <verda.paco@protonmail.com>
2020-08-12 10:07:02 +0300

    link and backgroud update - update js link animation - change image on progressive jpeg

--- main.js
+++ main.js
@@ -1,16 +1,7 @@
 'use strict';
 
-function ChangeHTMLHeight () {
-    //this js code needs to be replaced on css or html or anything for browsers without js
-    if (document.documentElement.clientHeight < document.documentElement.scrollHeight) {document.getElementsByTagName("html")[0].style.height = "auto"; }
-}
-
 document.getElementById('hider').onclick = function toggle() {
     let el = document.getElementById('hide');
-    el.style.display = (el.style.display === 'none') ? '' : 'none';
-    ChangeHTMLHeight();
+    el.style.height = (el.style.height === '0px') ? '32px' : '0px';
 };
-
-
-document.addEventListener("DOMContentLoaded", ChangeHTMLHeight());
                           
