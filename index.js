b1e0315b41de5483653f0df2f66ddd7f80491e94
besdar <besdar@protonmail.com>
2021-07-04 16:26:49 +0300

    fix console errors

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
 
-document.getElementById('hider').onclick = function toggle() {
-    let el = document.getElementById('hide');
-    el.style.height = (el.style.height === '0px') ? el.children[0].offsetHeight + 'px' : '0px';
-};
+() => {
+    // none
+}
                           
