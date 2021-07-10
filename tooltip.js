59190ab63e62f8df16041a61629aadfabea3977b
besdar <besdar@protonmail.com>
2021-07-10 15:07:19 +0300

    Made english version of the CV 1.0

--- tooltip.js
+++ tooltip.js
@@ -1,20 +1,37 @@
 'use strict';
 
-const tipTexts = {
-    'softwise-stack__js': 'ES5 и ES6+, TS, участие в доработке линтера и таск-ранннера, написание кода правил workflow в изолированной среде YouTrack, решение разноплановых задач (как доработка сайтов, так и доработка невидимых рекламных скриптов) и много чего ещё',
-    'softwise-stack__jquery': 'Переписывал легаси код на современный стек или дорабатывал его функциональность.',
-    'softwise-stack__htmlcss': 'Учавствовал в обсуждении стандартов компании в HTML и CSS, делал рефактор верстки в phtml.',
-    'softwise-stack__php': 'Дорабатывал и делал рефактор существующего кода согласно современным стандартам компании, но без архитектурных изменений.',
-    'softwise-stack__sql': 'Доработка функций, написание запросов на обновление и получение каких-либо данных.',
-    'softwise-stack__react': 'Производил большой рефакторинг существующего кода в том числе архитектурный, писал объемные модули на несколько тысяч строк.',
-    'softwise-stack__vue': 'Производил небольшие доработки существующего функционала, брался за Vue и успешно с ним справлялся в положенные сроки при том, что ранее вообще не имел опыта работы с Vue.',
-    'bios-stack__react': 'Создавал приложения с чистого листа, сам продумывал архитектуру, разбирал и серьезно дорабатывал (переделывал) существующие проекты, активно использовал TS.',
-    'bios-stack__htmlcss': 'Поддерживал несколько различных сайтов, делал серьезный рефактор как CSS, так и HTML. Писал новые страницы и модули для этих сайтов.',
-    'bios-stack__js': 'Использовал чистый JS (+TS) как для написания React приложений, так и для доработки нескольких веб-сайтов.',
-    'bios-stack__php': 'Поддержка и доработка существующего легаси-кода, взаимодействие с API 1С-Битрикс, написание небольших php-скриптов, обновление версии PHP.',
-    'bios-stack__nodejs': 'Писал серверный JS код для React-приложения, работал с кофигом webpack и babel, использовал npm как таск-раннер так и пакетный менеджер.',
-    'bios-stack__1c': 'Как веб-разработчик работал с 1С-Битрикс. Помимо прочей работы в 1С писал много серверного кода для взаимодействия 1С с вебом, API разных приложений (например, Telegram).',
-};
+const language = document.documentElement.lang;
+const tipTexts = {};
+
+if (language === 'ru') {
+    tipTexts['bios-stack__1c'] = 'Как веб-разработчик работал с 1С-Битрикс. Помимо прочей работы в 1С писал много серверного кода для взаимодействия 1С с вебом, API разных приложений (например, Telegram).';
+    tipTexts['bios-stack__htmlcss'] = 'Поддерживал несколько различных сайтов, делал серьезный рефактор как CSS, так и HTML. Писал новые страницы и модули для этих сайтов.';
+    tipTexts['bios-stack__js'] = 'Использовал чистый JS (+TS) как для написания React приложений, так и для доработки нескольких веб-сайтов.';
+    tipTexts['bios-stack__nodejs'] = 'Писал серверный JS код для React-приложения, работал с кофигом webpack и babel, использовал npm как таск-раннер так и пакетный менеджер.';
+    tipTexts['bios-stack__php'] = 'Поддержка и доработка существующего легаси-кода, взаимодействие с API 1С-Битрикс, написание небольших php-скриптов, обновление версии PHP.';
+    tipTexts['bios-stack__react'] = 'Создавал приложения с чистого листа, сам продумывал архитектуру, разбирал и серьезно дорабатывал (переделывал) существующие проекты, активно использовал TS.';
+    tipTexts['softwise-stack__htmlcss'] = 'Учавствовал в обсуждении стандартов компании в HTML и CSS, делал рефактор верстки в phtml.';
+    tipTexts['softwise-stack__jquery'] = 'Переписывал легаси код на современный стек или дорабатывал его функциональность.';
+    tipTexts['softwise-stack__js'] = 'ES5 и ES6+, TS, участие в доработке линтера и таск-ранннера, написание кода правил workflow в изолированной среде YouTrack, решение разноплановых задач (как доработка сайтов, так и доработка невидимых рекламных скриптов) и много чего ещё.';
+    tipTexts['softwise-stack__php'] = 'Дорабатывал и делал рефактор существующего кода согласно современным стандартам компании, но без архитектурных изменений.';
+    tipTexts['softwise-stack__react'] = 'Производил большой рефакторинг существующего кода в том числе архитектурный, писал объемные модули на несколько тысяч строк.';
+    tipTexts['softwise-stack__sql'] = 'Доработка функций, написание запросов на обновление и получение каких-либо данных.';
+    tipTexts['softwise-stack__vue'] = 'Производил небольшие доработки существующего функционала, брался за Vue и успешно с ним справлялся в положенные сроки при том, что ранее вообще не имел опыта работы с Vue.';
+} else if (language === 'en') {
+    tipTexts['bios-stack__1c'] = 'As a frontend developer was working with 1C-Bitrix. Among other things was writing a lot of 1C server side code for working with web protocols, API different applications (like Telegram for example).';
+    tipTexts['bios-stack__htmlcss'] = 'Supported several different sites, made serious refactor for CSS and HTML. Wrote new pages and modules for this sites.';
+    tipTexts['bios-stack__js'] = 'Use vanilla JS (+TS) as for React applications as well for rework several sites.';
+    tipTexts['bios-stack__nodejs'] = 'Wrote server side JS code for React application, worked with webpack and babel configurations, used npm as task-runner as well package manager.';
+    tipTexts['bios-stack__php'] = 'Support and rewrite exist legacy code, interact with 1C-Bitrix API, writing small php-scripts, updating PHP version.';
+    tipTexts['bios-stack__react'] = 'Created application from scratch, analyze application architecture by myself, analyzed and seriously rewrite (rework) exist projects, actively used TS.';
+    tipTexts['softwise-stack__htmlcss'] = 'Take part in the HTML and CSS code style standarts discussions, made refactoring html layout in the phtml files.';
+    tipTexts['softwise-stack__jquery'] = 'Rewrite legacy code to modern language stack or reworked its functionality';
+    tipTexts['softwise-stack__js'] = 'ES5 and ES6+, TS, took part in the JS linter and task-runner (gulp) reworking, wrote rules for YouTrack workflows, solved the most different tasks (as the site reworking as well reworking invisible advertising scripts) and a lot of other things.';
+    tipTexts['softwise-stack__php'] = 'Rewrote and made refactoring existing code according company standarts but without architecture changes.';
+    tipTexts['softwise-stack__react'] = 'Made large refactoring existing code including architecture refactoring, wrote large modules for thousands rows.';
+    tipTexts['softwise-stack__sql'] = 'Rewriting functions, writing queries for updating and getting different data.';
+    tipTexts['softwise-stack__vue'] = 'Made small fixes existing functionality, took Vue and succesfully handle task in the right time despite the fact that I have not experience with Vue earlier.';
+}
 
 const tooltipElement = document.getElementsByClassName('aside-content__tooltip')[0];
 let isClicked = false;
@@ -58,7 +75,7 @@ const createTip = (event, isHover = false) => {
     } else if (leftPos + tooltipWidth > window.innerWidth - windowPadding) {
         leftPos = window.innerWidth - windowPadding - tooltipWidth;
     }
-    
+
     tooltipElement.style.top = topPos + 'px';
     tooltipElement.style.left = leftPos + 'px';
     isClicked = !isHover;
