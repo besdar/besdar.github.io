2128c2902774a0203361a8ffe0b192ea5cf57da0
Dmitry <43372966+besdar@users.noreply.github.com>
2025-02-14 12:24:47 +0000

    Updates

--- index.d7ad677f.js
+++
@@ -1,2 +0,0 @@
-document.querySelectorAll(".tech-stack-item").forEach(e=>{e.addEventListener("focus",({currentTarget:e})=>{if(window.innerWidth<700)return;let t=e?.querySelector(".tooltip");if(!t)return;let i=t.getBoundingClientRect();i.x<10?t.style.left=`${t.offsetLeft+10-i.x}px`:i.x+i.width>window.innerWidth-10&&(t.style.left=`${window.innerWidth+t.offsetLeft-20-i.x-i.width}px`)})});
-//# sourceMappingURL=index.d7ad677f.js.map
